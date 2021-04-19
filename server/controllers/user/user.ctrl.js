362128b1fb07c6066ac8fe3eba022f66c34c41d9
Sungjin <esung246@gmail.com>
2021-04-19 09:17:45 +0900

    [Fixed] change id from user_id

--- server/controllers/user/user.ctrl.js
+++ server/controllers/user/user.ctrl.js
@@ -1,10 +1,9 @@
-const { useLayoutEffect } = require('react');
 const getConnection = require('../../models');
 
 exports.post_signup = ( req, res ) => {
 
     getConnection((connection) => {
-        connection.query(`insert ignore into user (user_id, password, admin) values('${req.body.user_id}', '${req.body.password}', false);`, function(err, rows) {
+        connection.query(`insert ignore into user (id, password, admin) values('${req.body.user_id}', '${req.body.password}', false);`, function(err, rows) {
             if(err) {
                 console.log({message: 'user is exist so insert failed'});
                 res.status(404).send({message: 'user is exist so insert failed'});
@@ -23,7 +22,7 @@ exports.post_signin = (req, res) => {
 
     getConnection((connection) => {
 
-        connection.query(`select password, admin from user where user_id = '${req.body.user_id}';`, function(err, result) {
+        connection.query(`select password, admin from user where id = '${req.body.user_id}';`, function(err, result) {
             if(!err) {
                 if(result[0].password == req.body.password) {
                     if(result[0].admin) {
@@ -36,8 +35,8 @@ exports.post_signin = (req, res) => {
                     res.status(404).send({message: 'password is not correct'});
                 }
             } else {
-                console.log({message: 'user_id is not exist'});
-                res.status(404).send({message: 'user_id is not exist'});
+                console.log({message: 'id is not exist'});
+                res.status(404).send({message: 'id is not exist'});
             }
         })
 
@@ -49,13 +48,13 @@ exports.post_forgot = (req, res) => {
 
     getConnection((connection) => {
 
-        connection.query(`update user set password='${req.body.password}' where user_id='${req.body.user_id}' and admin=false;`, function(err) {
+        connection.query(`update user set password='${req.body.password}' where id='${req.body.user_id}' and admin=false;`, function(err) {
             if(!err) {
                 console.log({message: 'reset password : success'});
                 res.status(200).send();
             } else {
-                console.log({message: 'user_id is not exist'});
-                res.status(404).send({message: 'user_id is not exist'});
+                console.log({message: 'id is not exist'});
+                res.status(404).send({message: 'id is not exist'});
             }
         })
 
